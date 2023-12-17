da50825bca1f747d8c5a80d4e0f1ba7ab42e3331
Ackson94 <acksonicmutuma@gmail.com>
2023-12-17 10:34:28 +0200

    otp

---
+++ lib/jobber/application.change2.ex
@@ -0,0 +1,19 @@
+defmodule Jobber.Application do
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    job_runner_config = [
+      strategy: :one_for_one,
+      max_seconds: 30_000,
+      name: Jobber.JobRunner
+    ]
+    children = [
+     {Registry, keys: :unique, name: Jobber.JobRegistry},
+     {DynamicSupervisor, job_runner_config}
+    ]
+
+    opts = [strategy: :one_for_one, name: Jobber.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
